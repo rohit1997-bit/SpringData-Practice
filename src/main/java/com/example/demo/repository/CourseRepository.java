f91d3931c877c30eab7f0547291beb997fad6cbb
rohith <65489033+rohit1997-bit@users.noreply.github.com>
2025-10-20 18:15:59 +0530

    updated course class

--- src/main/java/com/example/demo/repository/CourseRepository.java
+++ src/main/java/com/example/demo/repository/CourseRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CoursesRepository extends CrudRepository<Courses,Integer> {
+public interface CourseRepository extends CrudRepository<Courses,Integer> {
 }
