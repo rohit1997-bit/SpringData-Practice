f034bd535acad4a956b123d2fb9cdd02e6d59aa5
rohith <65489033+rohit1997-bit@users.noreply.github.com>
2025-10-20 15:30:06 +0530

    little update on the sprind data jpa practice

--- src/main/java/com/example/demo/repository/StudentsRepository.java
+++ src/main/java/com/example/demo/repository/StudentsRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface StudentRepository extends CrudRepository<Student,Integer> {
+public interface StudentsRepository extends CrudRepository<Student,Integer> {
 
       Student findByPerson_Name(String name);
 }
